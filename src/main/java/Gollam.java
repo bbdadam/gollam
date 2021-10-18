02a660d99173e67d03ca4eea9454db5c7992845d
AdamBabud <adam.babud@gmail.com>
2021-10-18 21:35:31 +0200

    adding StealthyLittleHands some meaning

--- src/main/java/Gollam.java
+++ src/main/java/Gollam.java
@@ -4,18 +4,41 @@ import java.util.List;
 import java.util.Set;
 
 public class Gollam {
-
     private static final int MAX_PAGES_TO_SEARCH = 10;
     private Set<String> pagesVisited = new HashSet<String>();
-    //Using LinkedList to implement breadth-first search
     private List<String> pagesToVisit = new LinkedList<String>();
 
-    private String nextUrl(){
+
+    public void search(String url, String searchWord) {
+        while (this.pagesVisited.size() < MAX_PAGES_TO_SEARCH) {
+            String currentUrl;
+            StealthyLittleHands hands = new StealthyLittleHands();
+            if (this.pagesToVisit.isEmpty()) {
+                currentUrl = url;
+                this.pagesVisited.add(url);
+            } else {
+                currentUrl = this.nextUrl();
+            }
+            hands.crawl(currentUrl); // Lots of stuff happening here. Look at the crawl method in
+            // SpiderLeg
+            boolean success = hands.searchForWord(searchWord);
+            if (success) {
+                System.out.println(String.format("**Success** Word %s found at %s", searchWord, currentUrl));
+                break;
+            }
+            this.pagesToVisit.addAll(hands.getLinks());
+        }
+        System.out.println("\n**Done** Visited " + this.pagesVisited.size() + " web page(s)");
+    }
+
+
+    private String nextUrl() {
         String nextUrl;
         do {
-          nextUrl = this.pagesToVisit.remove(0);
+            nextUrl = this.pagesToVisit.remove(0);
         } while (this.pagesVisited.contains(nextUrl));
         this.pagesVisited.add(nextUrl);
         return nextUrl;
     }
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Gollam gollam = new Gollam();
+        gollam.search("https://www.hasznaltauto.hu/#personalVehicleSearchBox.searchParameters", "MITSUBISHI");
     }
 }
